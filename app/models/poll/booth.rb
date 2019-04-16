consuldemocracy/consuldemocracy

Fix queries and scopes after column deletion
Some queries were accessing original column instead of the new
translatable one. This should have been causing unexpected behavior
for requests maded in a different locale than the application default.

app/models/poll/booth.rb
@@ -12,7 +12,7 @@ class Poll
     end
 
     def self.available
-      where(polls: { id: Poll.current_or_recounting_or_incoming }).includes(:polls)
+      where(polls: { id: Poll.current_or_recounting_or_incoming }).joins(polls: :translations)
     end
 
     def assignment_on_poll(poll)

manuchis/consul

app/models/poll/booth.rb
@@ -12,7 +12,7 @@ class Poll
     end
 
     def self.available
-      where(polls: { id: Poll.current_or_recounting }).includes(:polls)
+      where(polls: { id: Poll.current_or_recounting }).joins(polls: :translations)
     end
 
     def assignment_on_poll(poll)